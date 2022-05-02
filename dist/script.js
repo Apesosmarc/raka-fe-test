01ab1167794be3caf153ebc651a0bf673bc2297b
Marc Apesos <apesos2013@gmail.com>
2022-05-02 12:33:38 -0400

    copyright script added
    changes year from js Date

--- dist/script.js
+++ dist/script.js
@@ -1,2 +1,2 @@
-"use strict";console.log("js loading");var closeButton=document.querySelector(".close-nav"),openButton=document.querySelector(".open-nav"),nav=document.querySelector(".nav");function redirect(){setTimeout((function(){window.location.replace("/")}),5e3)}closeButton.addEventListener("click",(function(){nav.classList.remove("navigation-open")})),openButton.addEventListener("click",(function(){nav.classList.add("navigation-open")})),document.body.addEventListener("onload",redirect());
+"use strict";var getYear=function(){var e=document.querySelector(".footer__fineprint"),t=(new Date).getFullYear();e.innerHTML="© ".concat(t," Raka. All Rights Reserved")};document.addEventListener("onload",getYear());
 //# sourceMappingURL=script.js.map
--- js/script.js
+++ js/script.js
@@ -1,21 +1,22 @@
-console.log("js loading");
+// console.log("js loading");
 
-const closeButton = document.querySelector(".close-nav");
-const openButton = document.querySelector(".open-nav");
-const nav = document.querySelector(".nav");
+// const closeButton = document.querySelector(".close-nav");
+// const openButton = document.querySelector(".open-nav");
+// const nav = document.querySelector(".nav");
 
-closeButton.addEventListener("click", () => {
-  nav.classList.remove("navigation-open");
-});
+// closeButton.addEventListener("click", () => {
+//   nav.classList.remove("navigation-open");
+// });
 
-openButton.addEventListener("click", () => {
-  nav.classList.add("navigation-open");
-});
+// openButton.addEventListener("click", () => {
+//   nav.classList.add("navigation-open");
+// });
 
-function redirect() {
-  setTimeout(function () {
-    window.location.replace("/");
-  }, 5000);
-}
+const getYear = () => {
+  const copyright = document.querySelector(".footer__fineprint");
+  const year = new Date().getFullYear();
 
-document.body.addEventListener("onload", redirect());
+  copyright.innerHTML = `© ${year} Raka. All Rights Reserved`;
+};
+
+document.addEventListener("onload", getYear());
